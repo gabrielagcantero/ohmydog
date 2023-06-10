16f9c878bdd036968833fc285d1668a733a3e48d
gcantero <gabrielagcantero@gmail.com>
2023-06-10 14:10:07 -0300

    ver lista de perros del cliente funcionando

--- ohmyapp-react/src/bodyParts/cliente/perros.js
+++ ohmyapp-react/src/bodyParts/cliente/perros.js
@@ -75,7 +75,7 @@ function Perros(){
             <div className="card-wrapper">
                 <div className="card-box align-center">
                     <div className="iconfont-wrapper">
-                        <span className="mbr-iconfont mobi-mbri-hearth mobi-mbri" onClick={muestraDogs}></span>
+                        <span className="mbr-iconfont mbrib-home" onClick={muestraDogs}></span>
                     </div>
                     <h5 className="card-title mbr-fonts-style display-7"><strong>Mis Perros</strong></h5>
                     <p className="card-text mbr-fonts-style display-7">Vea qué perros tiene registrados.</p>
--- ohmyapp-react/src/bodyParts/veterinario/clients.js
+++ ohmyapp-react/src/bodyParts/veterinario/clients.js
@@ -12,18 +12,65 @@ function getClients(){
     return mails;
 }
 
+//trae los perros y los devuelve en un array
+function getDogs(){
+    const dogs = [];
+
+    fetch('http://localhost:3000/get-ownerdata')
+        .then((response) => response.json())
+        .then((results) => {results.map((e) => dogs.push(e))});
+    
+    return dogs;
+}
+
 let clients = getClients();
+let dogs = getDogs();
 
 function Clients(){
     let[showClients, setShowClients] = useState(false);
     let[showCLientData, setShowClientData] = useState(null);
+    let[showDogs, setShowDogs] = useState(null);
 
     //muestra/oculta los clientes
     const muestraClients = () => {
         setShowClients(!showClients);         
     }
 
-    const clientData = (mail) => {
+    //muestra la lista de perros de un cliente
+    const dogList = (showDogs,setShowDogs) => {
+        let l = dogs.filter((d) => d.owner === showDogs);
+        let children;
+        //si hay perros devuelve la lista
+        if (l.length > 0){ 
+            children = l.map((e) => {
+                return( 
+                    <div className="col-10" style={{marginLeft: "30px"}}>
+                        <h6 className="card-title2 mbr-fonts-style m-0 mb-3 display-4">
+                            <button value={e.id} className="btn btn-success" >Ver perro</button>
+                            <strong>{e.name}</strong> 
+                        </h6>
+                    </div>
+                )})}
+        //si no hay clientes devuelve mensaje
+        else{ 
+            children = (
+                <div className="col-12 col-md" style={{marginLeft: "30px"}}>
+                    <h6 className="card-title2 mbr-fonts-style m-0 mb-3 display-4">
+                        <strong>En este momento el cliente no posee perros registrados en el sistema</strong> 
+                    </h6>
+                </div>
+        )}
+        
+        return children;
+    }
+
+
+    //muestra los datos del cliente
+    const clientData = (mail, showDogs, setShowDogs) => {
+
+        //muestra/oculta los perros del cliente
+        const mostrarListaP = (event) => {showDogs? setShowDogs(null) : setShowDogs(event.target.value)};
+
         let c = clients.find((p) => p.mail === mail);
         return (
             <div>
@@ -42,16 +89,17 @@ function Clients(){
                 <h6 className="card-title2 mbr-fonts-style mb-3 display-7">
                     <strong>Teléfono: {c.tel} </strong> 
                 </h6>
-                <button value={c.mail} className="btn btn-success">Ver perros de {c.frist_name}</button>
-    
+                <button value={c.mail} className="btn btn-success" onClick={mostrarListaP}>Ver perros de {c.frist_name}</button>
+                {c.mail ===  showDogs && (dogList(showDogs, setShowDogs))}
+                <br/><br/>
             </div>
         )
     }
     
-    const clientList = (showCLientData, setShowClientData) => {
+    const clientList = (showCLientData, setShowClientData, showDogs,setShowDogs) => {
         let children;
     
-        //muestra los datos del cliente
+        //muestra/oculta los datos del cliente
         const mostrarCliente = (event) => {showCLientData? setShowClientData(null) : setShowClientData(event.target.value)};
     
         //si hay perros devuelve la lista
@@ -63,7 +111,7 @@ function Clients(){
                             <button value={e.mail} className="btn btn-success" onClick={mostrarCliente}>Ver Cliente</button>
                             <strong>{e.mail}</strong> 
                         </h6>
-                        {e.mail ===  showCLientData && clientData(e.mail)}
+                        {e.mail ===  showCLientData && clientData(e.mail, showDogs, setShowDogs)}
                     </div>
                 )})}
         //si no hay clientes devuelve mensaje
@@ -91,7 +139,7 @@ function Clients(){
                         <div >
                             <div className="card-wrapper">
                                 <div className="row align-items-center">
-                                        {clientList(showCLientData, setShowClientData)}
+                                        {clientList(showCLientData, setShowClientData, showDogs, setShowDogs)}
                                 </div>
                             </div>
                         </div>
